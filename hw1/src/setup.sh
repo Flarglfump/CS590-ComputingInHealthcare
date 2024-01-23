72013bc2c94b51e669c8b59c0551f8153fb2460b
Flarglfump <witskeng@yahoo.com>
2024-01-23 12:18:48 -0600

    setup.sh - added code to init conda env
    - Doesn't work yet

--- hw1/src/setup.sh
+++ hw1/src/setup.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
+# Set up vars
+MINICONDA_DIR="$HOME/miniconda3"
+CONDA_EXE="$MINICONDA_DIR/bin/conda"
+CONDA_ENV_NAME="gwitske_env"
+
 # Update environment, install htop and screen
 sudo apt-get update
 sudo apt-get install -y htop screen
 
 # Setup miniconda
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
+mkdir -p $MINICONDA_DIR
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O $MINICONDA_DIR/miniconda.sh
+bash $MINICONDA_DIR/miniconda.sh -b -u -p $MINICONDA_DIR
+rm -rf $MINICONDA_DIR/miniconda.sh
 
-if test ! -f ~/miniconda3/bin/conda; then
+source "$HOME/.bashrc"
+if test ! -f $CONDA_EXE; then
   echo "Could not find conda executable"
+  exit
 else
   echo "Initializing conda bash environment..."
-  export PATH="~/miniconda3/bin:$PATH"
-  conda init bash
+  $CONDA_EXE init bash
 fi
 
-# Create, activate and export conda environment
-conda create --name gwitske_env python=3.11 -y
-source ~/.bashrc
-conda activate gwitske_env
+echo "Env Name: "$CONDA_ENV_NAME
+# Create/activate conda environment
+if $CONDA_EXE env list | grep -q $CONDA_ENV_NAME;
+then
+  echo "Environment $CONDA_ENV_NAME exists"
+else
+  echo "Environment $CONDA_ENV_NAME will be created..."
+  $CONDA_EXE create --name $CONDA_ENV_NAME python=3.11 -y
+fi
+
+$CONDA_EXE init bash
+$CONDA_EXE activate $CONDA_ENV_NAME
