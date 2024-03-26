9cc3fef0c594d76c613b380d1142f69b5e9aeda4
Flarglfump <witskeng@yahoo.com>
2024-03-26 13:33:33 -0500

    p2 - Added URLs for T cells to fetch script

---
+++ p2/fetchData.sh
@@ -0,0 +1,48 @@
+condaEnvName="CS590-p2"
+
+TCELL_FILE_URLS=(
+  "https://www.encodeproject.org/files/ENCFF105ZAX/@@download/ENCFF105ZAX.bed.gz" # 1
+  "https://www.encodeproject.org/files/ENCFF273DGN/@@download/ENCFF273DGN.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF662SIF/@@download/ENCFF662SIF.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF049DNM/@@download/ENCFF049DNM.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF792YKT/@@download/ENCFF792YKT.bed.gz" # 5
+  "https://www.encodeproject.org/files/ENCFF929NTI/@@download/ENCFF929NTI.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF705AZV/@@download/ENCFF705AZV.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF123QHV/@@download/ENCFF123QHV.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF685MCV/@@download/ENCFF685MCV.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF754SOL/@@download/ENCFF754SOL.bed.gz" # 10
+  "https://www.encodeproject.org/files/ENCFF211AMR/@@download/ENCFF211AMR.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF955PTL/@@download/ENCFF955PTL.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF555UEL/@@download/ENCFF555UEL.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF867TMP/@@download/ENCFF867TMP.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF628SES/@@download/ENCFF628SES.bed.gz" # 15
+  "https://www.encodeproject.org/files/ENCFF706APW/@@download/ENCFF706APW.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF349KLY/@@download/ENCFF349KLY.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF364QVY/@@download/ENCFF364QVY.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF026XCY/@@download/ENCFF026XCY.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF117WNZ/@@download/ENCFF117WNZ.bed.gz" # 20
+  "https://www.encodeproject.org/files/ENCFF036OKE/@@download/ENCFF036OKE.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF456OPK/@@download/ENCFF456OPK.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF754GUE/@@download/ENCFF754GUE.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF845NWD/@@download/ENCFF845NWD.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF788BUI/@@download/ENCFF788BUI.bed.gz" # 25
+  "https://www.encodeproject.org/files/ENCFF089LSF/@@download/ENCFF089LSF.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF058UYY/@@download/ENCFF058UYY.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF792EFB/@@download/ENCFF792EFB.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF980APH/@@download/ENCFF980APH.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF035XXI/@@download/ENCFF035XXI.bed.gz" # 30
+  "https://www.encodeproject.org/files/ENCFF246KRE/@@download/ENCFF246KRE.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF095ATB/@@download/ENCFF095ATB.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF381CVI/@@download/ENCFF381CVI.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF541PYT/@@download/ENCFF541PYT.bed.gz" 
+  "https://www.encodeproject.org/files/ENCFF721ZMU/@@download/ENCFF721ZMU.bed.gz" # 35
+  "https://www.encodeproject.org/files/ENCFF547LIL/@@download/ENCFF547LIL.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF350JTS/@@download/ENCFF350JTS.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF418SHH/@@download/ENCFF418SHH.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF739WIV/@@download/ENCFF739WIV.bed.gz" 
+  "https://www.encodeproject.org/files/ENCFF570EVR/@@download/ENCFF570EVR.bed.gz" # 40
+  "https://www.encodeproject.org/files/ENCFF025DBX/@@download/ENCFF025DBX.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF314YTZ/@@download/ENCFF314YTZ.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF267DDX/@@download/ENCFF267DDX.bed.gz"
+  "https://www.encodeproject.org/files/ENCFF354VIQ/@@download/ENCFF354VIQ.bed.gz" # 44
+)
